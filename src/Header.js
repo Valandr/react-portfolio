ff93545d5bd62eee1e629e0df34a70f51003ad04
Andriani <valerie.andriani@gmail.com>
2020-03-03 17:35:42 +0100

    Create and Styling Header component

---
+++ src/Header.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import Profile from './profile.jpg'
+import './Header.css'
+
+const heroImage = {
+    backgroundImage: `url(${Profile})`,
+    height: '50vh',
+    backgroundSize: 'cover'
+}
+class Header extends Component {
+    render() {
+        return (
+            <header style={heroImage}>
+                <h1>{this.props.title}</h1>
+                <p>Développeuse Front-end.</p>
+                <a href="#button">Mes Réalisations</a>
+            </header>
+        )
+    }
+}
+
+export default Header
--- src/index.js
+++ src/index.js
@@ -2,10 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 // import './index.css';
 import Navigation from './Navigation';
+import Header from './Header'
 // import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<Navigation logoTitle="Valérie ANDRIANI" />, document.getElementById('root'));
+class App extends React.Component {
+    render() {
+        return(
+            <div>
+                <Navigation logoTitle="Valérie ANDRIANI" />
+                <Header title="WELCOME"/>
+            </div>
+            
+        )
+    }
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
